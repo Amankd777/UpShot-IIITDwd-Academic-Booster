9b6c57526e48916291d81d2fc3b1f9c750fc2a18
Aman Vignesh <20bds003@iiitdwd.ac.in>
2022-03-14 22:10:08 +0530

    Modal box add and Dark mode Toggle update

--- index.js
+++ index.js
@@ -1,7 +1,45 @@
-const btn = document.querySelector('.fa-moon');
 
-btn.addEventListener('click', function() {
-  document.body.classList.toggle('dark-mode');
+// Toggle dark mode 
+const checkbox = document.querySelector("#checkbox");
+
+checkbox.addEventListener("change", function(){
+    document.body.classList.toggle("dark-mode");
+    document.querySelector("#navbar-mod").classList.toggle("bg-light");
+    document.querySelector("#navbar-mod").classList.toggle("navbar-light");
+  }
+)
+
+// Material Select Initialization
+// pyq modal box
+var modalpyq = $("#pyq");
+$(".modal-box-1").click(function(){
+  modalpyq.show();
+  $("body").css("overflow","hidden");
 })
+$(".close").click(function(){
+  modalpyq.hide();
+  $("body").css("overflow","visible");
+});
 
-// This is a sample Comment
+// model modal box
+var modalmodel = $("#model");
+$(".modal-box-2").click(function(){
+  modalmodel.show();
+  $("body").css("overflow","hidden");
+});
+$(".close").click(function(){
+  modalmodel.hide();
+  $("body").css("overflow","visible");
+});
+
+// notes modal box
+var modalnotes = $("#notes");
+$(".modal-box-3").click(function(){
+  modalnotes.show();
+  $("body").css("overflow","hidden");
+});
+$(".close").click(function(){
+  modalnotes.hide();
+  $("body").css("overflow","visible");
+});
+
